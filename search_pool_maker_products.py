b291dd24e8531131a58d58636f99d63a9c42aefc
matiasacunha95 <matiasacunha95@gmail.com>
2022-04-07 12:59:16 -0400

    [FIX] search pool generator for products

--- search_pool_maker_products.py
+++ search_pool_maker_products.py
@@ -4,30 +4,28 @@ from search_pool_maker_ingredients import *
 
 filename = 'foods'
 
-def title_spliter_foods(title, brand):
+def title_spliter_foods(title):
     #'Returns an array with a search pool for each dictionary'
     secuence = []
-    brand_secuence = []
     i = 0
     for letter in title:
         if len(secuence) > 0:
             secuence.append(secuence[i - 1] + letter)
         else:
             secuence.append(letter)
-    for letter in brand:
-        if len(brand_secuence) > 0:
-            brand_secuence.append(brand_secuence[i - 1] + letter)
-        else:
-            brand_secuence.append(letter)
-    secuence.extend(brand_secuence)
-    return list(dict.fromkeys(secuence)) 
+    return secuence
 
 def get_search_pool(data):
     #'Returns new data with a search pool for each dictionary'
     new_data = []
     for elem in data:
-        search_key_words = title_spliter_foods(elem['title'], elem['brand'])
-        elem['searchKeyWords'] = search_key_words
+        search_key_words = []
+        elem['price'] = 0
+        search_key_words += title_spliter_foods(elem['title'])
+        search_key_words += title_spliter_foods(elem['brand'])
+        for word in elem['title'].split(' '):
+            search_key_words += title_spliter_foods(word)
+        elem['searchKeyWords'] = list(dict.fromkeys(search_key_words))
         new_data.append(elem)
     return new_data
 
